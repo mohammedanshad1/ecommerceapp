7058d3cd08c0cee5df91458b91e2d4cbba2e17be
Anshad <anshadk556@gmail.com>
2024-10-01 12:43:15 +0530

    first commit

---
+++ android/app/src/main/java/com/example/innoitlabsmachintest/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.innoitlabsmachintest;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/core/constants/app_typography.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class appTypography {
+  static const String fontFamily = 'Outfit';
+
+  static const TextStyle black = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w900,
+  );
+
+  static const TextStyle bold = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle extraBold = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w800,
+  );
+
+  static const TextStyle extraLight = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w200,
+  );
+
+  static const TextStyle light = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w300,
+  );
+
+  static const TextStyle medium = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle regular = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semiBold = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle thin = TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w100,
+  );
+}
